b0e74b3025778685888168a2bee38cad99577c5a
ajipgt <financiaji@gmail.com>
2025-03-17 12:43:23 +0700

    modified:   src/config/index.ts

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,7 +3,7 @@ import { Github, Twitter } from "lucide-react"
 export const defaultLanguage: string = "en"
 
 export const common = {
-  domain: "https://astro-air.guoqi.dev",
+  domain: "https://ija.netlify.app",
   meta: {
     favicon: "/avatar.png",
     url: "https://blog.sunguoqi.com",
@@ -18,25 +18,19 @@ export const common = {
     {
       icon: Github,
       label: "GitHub",
-      link: "https://github.com/sun0225SUN",
+      link: "https://github.com/ajipgt",
     },
   ],
   rss: true,
   navigation: {
     home: true,
     archive: true,
-    custom: [
-      {
-        label: "CamLife",
-        link: "https://camlife.cn",
-      },
-    ],
     links: true,
     about: true,
   },
   latestPosts: 8,
   comments: {
-    enabled: true,
+    enabled: false,
     twikoo: {
       enabled: true,
       // replace with your own envId
@@ -45,76 +39,30 @@ export const common = {
   },
 }
 
-export const zh = {
+export const id = {
   ...common,
-  siteName: "小孙同学",
+  siteName: "Ija",
   meta: {
     ...common.meta,
-    title: "小孙同学",
-    slogan: "一个浪漫的理性主义者",
-    description: "读书、摄影、编程、旅行",
+    title: "Ija's Blog",
+    slogan: "Tempat serius untuk belajar",
+    description: "Matematika, Fisika, Kimia",
   },
   navigation: {
     ...common.navigation,
-    custom: [
-      {
-        label: "影集",
-        link: "https://camlife.cn",
-      },
-    ],
   },
   pageMeta: {
     archive: {
-      title: "归档",
-      description: "小孙同学的所有文章",
+      title: "Arsip",
+      description: "Semua Post",
       ogImage: "/images/page-meta/zh/archive.png",
     },
-    links: {
-      title: "朋友们",
-      description: "小孙同学的和他朋友们",
-      ogImage: "/images/page-meta/zh/links.png",
-    },
     about: {
-      title: "关于我",
-      description: "小孙同学的自我介绍",
+      title: "Tentang",
+      description: "Bio Penulis",
       ogImage: "/images/page-meta/zh/about.png",
     },
   },
 }
 
-export const en = {
-  ...common,
-  siteName: "Guoqi Sun",
-  meta: {
-    ...common.meta,
-    title: "Guoqi Sun",
-    slogan: "A Romantic Rationalist",
-    description: "Reading, Photography, Programming, Traveling",
-  },
-  navigation: {
-    ...common.navigation,
-    custom: [
-      {
-        label: "CamLife",
-        link: "https://camlife.cn",
-      },
-    ],
-  },
-  pageMeta: {
-    archive: {
-      title: "All Posts",
-      description: "Here are Guoqi Sun's all posts",
-      ogImage: "/images/page-meta/en/archive.png",
-    },
-    links: {
-      title: "My Friends",
-      description: "Here are Guoqi Sun's friends",
-      ogImage: "/images/page-meta/en/links.png",
-    },
-    about: {
-      title: "About Me",
-      description: "Here is Guoqi Sun's self-introduction",
-      ogImage: "/images/page-meta/en/about.png",
-    },
-  },
-}
+
